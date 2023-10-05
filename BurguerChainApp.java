95edaefaf3ec6b4b9f586c8cb1d9baf350250a17
abrenner22 <115625847+abrenner22@users.noreply.github.com>
2023-10-05 16:42:55 -0300

    Update BurguerChainApp.java
    Inserting the quiz description in the code comment section.

--- BurguerChainApp.java
+++ BurguerChainApp.java
@@ -1,19 +1,43 @@
-import java.util.Scanner; // importando o utilitário scanner padrão do java
+/* 
+A friend of yours was hired to create an application for a new burger restaurant chain that arrived in town. 
+It should calculate whether a customer should be given a gift card. The intention is that after the first 1000 customers, every 25th 
+should receive a gift card. In addition, every 2000th customer gets a larger gift card. 
+Others receive nothing (expect for the chance to buy the restaurant's servings, of course!).
+
+Currently, the code looks like this:
+
+Scanner x = new Scanner(System.in);
+System.out.print("Customer number: ");
+int y = Integer.valueOf(x.nextLine());
+
+if (y >= 1000 && y % 25 == 0) {
+    System.out.println("Gets a gift card!");
+} else if (y % 2000 == 0) {
+    System.out.println("Gets a large gift card!");
+} else {
+    System.out.println("Gets nothing.");
+}
+There are a few issues with it. Explain what problems exist in the code and what causes them. Also give suggestions to fix those.
+
+Your answer should be at least 20 words
+*/ 
+
+import java.util.Scanner; 
 
 public class BurguerChainApp { 
 
-    public static void main(String args[]) { // ponto de entrada do software
+    public static void main(String args[]) {
 
-        Scanner scan = new Scanner(System.in); // iniciando o objeto scanner
+        Scanner scan = new Scanner(System.in); 
 
-        while (true) { // loop para manter a usabildiade do programa 
+        while (true) { 
             
-            System.out.println("Insert a initial customer number: "); // insira um número inicial de cliente
-            int customerNumber = Integer.valueOf(scan.nextLine()); // salva o número inicial;
+            System.out.println("Insert a initial customer number: "); 
+            int customerNumber = Integer.valueOf(scan.nextLine());
             int smallGiftCard = 0;
             int bigGiftCard = 0;
             
-            for (int i = customerNumber; i < 10000; i++) { // laço for para gerar interatividade e teste
+            for (int i = customerNumber; i < 10000; i++) {
  
             if (customerNumber > 1000 && customerNumber % 25 == 0) {
                 System.out.println("Congratulations, cliente number " + customerNumber + ", you won a gift-card!");
